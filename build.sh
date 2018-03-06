cee423da0d0c5699be9ed108d8de97ed2a850e36
Jürgen Heiling <juergen.heiling@gmail.com>
2018-03-06 17:23:08 +0100

    added playground

--- build.sh
+++ build.sh
@@ -5,3 +5,5 @@ cd ../idris-js && sh ./build.sh && sh ./install.sh &&
 cd ../idris-electron && sh ./build.sh && sh ./install.sh &&
 cd ../idris-react && sh ./build.sh && sh ./install.sh &&
 cd ../idris-electron-react-scaffold && sh ./build.sh
+cd ../idris-playground && sh ./build.sh
+#cd ../idris-playground && idris --build view.ipkg
--- run.sh
+++ run.sh
@@ -2,3 +2,4 @@
 
 cd ../idris-electron && sh ./run_example.sh
 cd ../idris-electron-react-scaffold && sh ./run.sh
+cd ../idris-playground && sh ./run.sh
